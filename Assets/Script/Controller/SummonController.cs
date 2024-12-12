2686b7fd689597bc2b38633c6dc222fb7581ab4f
Hogoy0 <165356158+Hogoy0@users.noreply.github.com>
2024-12-12 21:10:40 +0900

    머지

--- Assets/Script/Controller/SummonController.cs
+++ Assets/Script/Controller/SummonController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SummonController : MonoBehaviour
@@ -11,11 +13,28 @@ public class SummonController : MonoBehaviour
     [HideInInspector] public Vector3 pos { get { return transform.position; } }
 
     public GameObject prefabToSpawn;
+    public GameObject prefabToSpawnMurge;
     public string targetLayerName = "Ground";
     private float health;
     private int attackPower;
     private CData cData;
 
+    public enum SlimeType
+    {
+        None,
+        Emerald_Battle,
+        Emerald_Battle_Murge,
+        Sapphire_Battle,
+        Sapphire_Battle_Murge,
+        Topaz_Battle,
+        Topaz_Battle_Murge,
+        Diamond_Battle,
+        Diamond_Battle_Murge,
+        Ruby_Battle,
+        Ruby_Battle_Murge,
+        
+    }
+    public SlimeType slimeType;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -67,20 +86,109 @@ public class SummonController : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer(targetLayerName))
         {
+            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z);
+            Vector3 MurgespawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            bool CheckRight = false;
+            bool CheckLeft = false;
+            SlimeType RightRayslimeType = SlimeType.None;
+            SlimeType LeftRayslimeType = SlimeType.None;
+            GameObject spawnedSlime = null;
+
             if (prefabToSpawn == null)
             {
                 return;
             }
-            Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z);
-            GameObject spawnedSlime = Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+
+            Vector3 rayOrigin = transform.position;
+            Vector3 rayDirection = Vector3.right;
+            float rayDistance = 1f;
+            Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.red, 2f);
+
+
+            RaycastHit2D Rightray = Physics2D.Raycast(rayOrigin, rayDirection, 1f, LayerMask.GetMask("Slime"));
+            RaycastHit2D Leftray = Physics2D.Raycast(rayOrigin, Vector3.left, 1f, LayerMask.GetMask("Slime"));
+
+            if (Rightray.collider != null)
+            {
+                Debug.Log("오른쪽에서 충돌 감지!");
+
+                // Ray에 맞은 오브젝트에서 SummonController를 가져오기
+                SummonController summonController = Rightray.collider.GetComponent<SummonController>();
+                if (summonController != null)
+                {
+                    // SummonController에서 slimeType 가져오기
+                    RightRayslimeType = summonController.slimeType;
+                    Debug.Log("충돌한 오브젝트의 SlimeType: " + RightRayslimeType);
+                    Debug.Log(Rightray.collider.name);
+                    CheckRight = true;
+                }
+                else
+                {
+                    Debug.LogWarning("충돌한 오브젝트에 SummonController가 없습니다.");
+                }
+            }
+
+            if (Leftray.collider != null)
+            {
+                Debug.Log("왼쪽에서 충돌 감지!");
+
+                // Ray에 맞은 오브젝트에서 SummonController를 가져오기
+                SummonController summonController = Leftray.collider.GetComponent<SummonController>();
+                if (summonController != null)
+                {
+                    // SummonController에서 slimeType 가져오기
+                    LeftRayslimeType = summonController.slimeType;
+                    Debug.Log("충돌한 오브젝트의 SlimeType: " + LeftRayslimeType);
+                    Debug.Log(Leftray.collider.name);
+                    CheckLeft = true;
+                }
+                else
+                {
+                    Debug.LogWarning("충돌한 오브젝트에 SummonController가 없습니다.");
+                }
+            }
+
+            if (CheckRight == true)
+            {
+                Debug.Log("던진 슬라임의 타입은" + slimeType);
+                Debug.Log("합쳐질 슬라임의 타입은" + RightRayslimeType.ToString());
+                if (slimeType == RightRayslimeType)
+                {
+                    Destroy(Rightray.collider.gameObject);
+                    spawnedSlime = Instantiate(prefabToSpawnMurge, MurgespawnPosition, Quaternion.identity);
+                    Destroy(gameObject);
+                }
+
+                else if (CheckLeft == true)
+                {
+                    if (slimeType == LeftRayslimeType)
+                    {
+                        Destroy(Leftray.collider.gameObject);
+                        spawnedSlime = Instantiate(prefabToSpawnMurge, MurgespawnPosition, Quaternion.identity);
+                        Destroy(gameObject);
+                    }
+                }
+            }
+
+            if (spawnedSlime == null) {
+                spawnedSlime = Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+                Destroy(gameObject);
+            }
             // 소환된 슬라임 초기화
+            
             SummonController newSlimeController = spawnedSlime.GetComponent<SummonController>();
             if (newSlimeController != null && cData != null)
             {
                 newSlimeController.Initialize(cData);
             }
+
+            
+            
             // 자기 자신을 제거
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
     }
+
+
+
 }
